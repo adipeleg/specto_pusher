9a8da97cdb8636cf313475d57699303a60ad30a1
adi <adifish21@gmail.com>
2017-06-30 11:40:17 +0300

    pass messages with payload to all listeners

--- web/channels/user_channel.ex
+++ web/channels/user_channel.ex
@@ -4,9 +4,23 @@ defmodule SpectoPusher.RoomChannel do
     def join("room:lobby", _message, socket) do
         {:ok, socket}
     end
+
     def join("room" <> _private_room_id, _params, socket) do
         {:ok, socket}
         #  {:error, %{reason: "unauthorized"}}
         # {:ok, reply, socket}
+    end
+
+    # broadcast!/3 will notify all joined clients on this
+    # socket's topic and invoke their handle_out/3 callbacks
+    def handle_in("new_msg", %{"body" => body}, socket) do
+        broadcast! socket, "new_msg", %{body: body}
+        {:noreply, socket}
+    end
+
+    def handle_out("new_msg", payload, socket) do
+        #here we can decide which messages to pass to all listeners 
+        push socket, "new_msg", payload
+        {:noreply, socket}
     end
 end
--- web/static/js/socket.js
+++ web/static/js/socket.js
@@ -55,6 +55,24 @@ socket.connect()
 
 // Now that you are connected, you can join channels with a topic:
 let channel = socket.channel("room:new", {})
+let chatInput         = document.querySelector("#chat-input")
+let messagesContainer = document.querySelector("#messages")
+
+chatInput.addEventListener("keypress", event => {
+  if(event.keyCode === 13){
+    //send the message with event name "new message"
+    //the bodt is the message payload
+    channel.push("new_msg", {body: chatInput.value}) 
+    chatInput.value = ""
+  }
+})
+
+channel.on("new_msg", payload => {
+  let messageItem = document.createElement("li");
+  messageItem.innerText = `[${Date()}] ${payload.body}`
+  messagesContainer.appendChild(messageItem)
+})
+
 channel.join()
   .receive("ok", resp => { console.log("Joined successfully", resp) })
   .receive("error", resp => { console.log("Unable to join", resp) })
